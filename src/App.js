ad26050853b137b7e83394788879db2d4b64b4fe
Brandon Saunders <BrandonNSaunders@gmail.com>
2019-03-20 21:25:15 -0700

    Adding links to my iOS apps.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import ProjectSelector from './components/project_selector/project_selector';
 
 let projects = [
     {
-        "name": "Tribogenics",
-        "description": "I worked at Tribogenics for a year."
+        "name": "Pet Photo iOS App",
+        "description": "Get your pet's attention when taking photos with other pet noises! Simply press the button of the respective pet sound and when your pet is looking at the camera tap anywhere on the screen. You'll see a small preview window in the bottom left corner of the screen. Tap the window to save the photo to your camera roll!",
+        "url": "https://itunes.apple.com/us/app/petphoto/id1178333966?mt=8"
     },
     {
-        "name": "Vantage LED",
-        "description": "I worked at Vantage LED for two years."
+        "name": "Multiplier Finder",
+        "description": "Learn or brush up on your multiplication tables with this iOS app. Similar to the traditional method of writing your multiplication tables and answers on flash card, you can practice for free. Select the set of times tables you would like to practice and swipe left and right to reveal the answer. Swipe down to return to the selection menu.",
+        "url": "https://itunes.apple.com/us/app/multiplier-finder/id1178339564"
     },
 ];
 
@@ -22,6 +24,7 @@ class App extends Component
             <div className="container">
                 <h1>Brandon Saunders</h1>
                 <p>Software Engineer who enjoys learning new things...</p>
+                <h3>Projects</h3>
                 <ProjectSelector {...projects[0]}/>
                 <ProjectSelector {...projects[1]}/>
             </div>
