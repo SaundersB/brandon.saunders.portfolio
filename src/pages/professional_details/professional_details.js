09f53058b8f08762236edfc6060e6e436b3952ca
Brandon Saunders <BrandonNSaunders@gmail.com>
2019-03-23 13:13:47 -0700

    Adding more information and a way for people to reach out to me.

--- src/pages/professional_details/professional_details.js
+++ src/pages/professional_details/professional_details.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Figure from 'react-bootstrap/es/Figure';
 import personal_profile_pic from '../../assets/images/profile_picture.png';
+import {Card} from 'react-bootstrap';
+import NavLink from 'react-bootstrap/NavLink';
 
 class ProfessionalDetails extends React.Component {
     render()
     {
         return(
             <div className="text-center maximize-window-minus-sidebar" >
-                <h2>About</h2>
                 <Figure>
                     <Figure.Image
                         roundedCircle={true}
@@ -17,8 +18,23 @@ class ProfessionalDetails extends React.Component {
                         src={personal_profile_pic}
                     />
                     <Figure.Caption>
-                        Currently working at JNR as a software engineer in the prepaid incentive market.
+
                     </Figure.Caption>
+                    <Card style={{ width: '35rem' }}>
+                        <Card.Body>
+                            <Card.Title>Introduction</Card.Title>
+                            <Card.Text className="left-align-text">
+                                Brandon Saunders is a software engineer with four years of consecutive software engineering experience in web development, application development, and DevOps.
+                                <br/><br/>
+                                Currently working at <Card.Link href="#/experience">JNR Incorporated</Card.Link> in Irvine, CA as a software engineer in the prepaid incentive market.
+                                Open to new opportunities for both full time and freelance work.
+                                <br/><br/>
+                                <div className="col text-center">
+                                    <Card.Link className="center-alignment" href="mailto:BrandonNSaunders@gmail.com">Contact me</Card.Link>
+                                </div>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
                 </Figure>
             </div>
         );
