9700d64d5ddf044e78d353fe0477682ecb9319ca
Brandon Saunders <BrandonNSaunders@gmail.com>
2019-03-23 07:59:04 -0700

    Remove the header in liu of side navigation

--- src/App.js
+++ src/App.js
@@ -4,24 +4,37 @@ import {
     Route,
     Switch
 } from "react-router-dom";
+import styled from "styled-components";
+import {
+    AppContainer as BaseAppContainer,
+    BodyContainer as Body
+} from "./containers/containers";
+
 import ProfessionalDetails from './pages/professional_details/professional_details';
 import NotFound from './pages/notfound/notfound';
-import Header from './components/header/header.js';
+import Navigation from './components/side_navigation/side_navigation.js';
 import Footer from './components/footer/footer.js';
+
+const AppContainer = styled(BaseAppContainer)`
+  height: calc(100vh - 40px);
+`;
+
 
 class App extends Component
 {
     render()
     {
         return (
-            <div>
-                <Header/>
-                <Switch>
-                    <Route path="/" component={ProfessionalDetails} />
-                    <Route component={NotFound} />
-                </Switch>
+            <AppContainer>
+                <Navigation/>
+                <Body>
+                    <Switch>
+                        <Route path="/" component={ProfessionalDetails} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </Body>
                 <Footer/>
-            </div>
+            </AppContainer>
         );
     }
 }
---
+++ src/components/side_navigation/side_navigation.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {SideNav, Nav} from 'react-sidenav';
+import styled from 'styled-components';
+import {NavigationContainer as BaseNavigation} from '../../containers/containers';
+
+const theme = {
+    selectionColor: '#FFF',
+    hoverBgColor: '#181b20',
+};
+const Text = styled.div`
+  padding-left: 8px;
+`;
+
+const Navigation = styled(BaseNavigation)`
+  background: #303641;
+  color: #8d97ad;
+  font-size: 1em;
+  letter-spacing: 2px;
+  width: 240px;
+  line-height: 22px;
+`;
+
+class SideNavigation extends React.Component
+{
+
+    render()
+    {
+        return (
+            <Navigation>
+                <SideNav
+                    defaultSelectedPath="1"
+                    theme={theme}
+                    onItemSelection={this.onItemSelection}>
+                    <Nav id="1">
+                        <Text>Dashboard</Text>
+                    </Nav>
+                    <Nav id="2">
+                        <Text>Users</Text>
+                    </Nav>
+                </SideNav>
+            </Navigation>
+        );
+    }
+}
+
+export default SideNavigation;
