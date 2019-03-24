7ce2ddbfd9a2177b0902cdd2226666c56c4fd4af
Brandon Saunders <BrandonNSaunders@gmail.com>
2019-03-23 18:06:53 -0700

    Utilize a mobile flyout menu option instead of the always visible desktop version.

--- src/components/side_navigation/side_navigation.js
+++ src/components/side_navigation/side_navigation.js
@@ -1,55 +1,34 @@
 import React from 'react';
-import {SideNav, Nav} from 'react-sidenav';
-import styled from 'styled-components';
-import {NavigationContainer as BaseNavigation} from '../../containers/containers';
+import './side_navigation.scss';
+import { slide as Menu } from 'react-burger-menu'
 import history from '../../router/history';
-
-const theme = {
-    selectionColor: '#FFF',
-    hoverBgColor: '#181b20',
-};
-const Text = styled.div`
-  padding-left: 8px;
-`;
-
-const Navigation = styled(BaseNavigation)`
-  background: #303641;
-  color: #8d97ad;
-  font-size: 1em;
-  letter-spacing: 2px;
-  height: auto;
-  width: 240px;
-  line-height: 22px;
-`;
 
 class SideNavigation extends React.Component
 {
-    state = { selectedPath: "1" };
-    onItemSelection = arg => {
-        this.setState({ selectedPath: arg.path });
-        console.log("State: " + arg.path);
-        switch (arg.path) {
-            case "about": {
+    handleClick = (path) => {
+        switch (path) {
+            case 'about': {
                 history.push('/about');
                 break;
             }
-            case "skills": {
+            case 'skills': {
                 history.push('/skills');
                 break;
             }
-            case "projects": {
+            case 'projects': {
                 history.push('/');
                 break;
             }
-            case "experience": {
+            case 'experience': {
                 history.push('/experience');
                 break;
             }
-            case "contact": {
+            case 'contact': {
                 window.location.href = "mailto:BrandonNSaunders@gmail.com";
                 break;
             }
             default: {
+                console.log("None");
                 break;
             }
         }
@@ -57,28 +36,12 @@ class SideNavigation extends React.Component
     render()
     {
         return (
-            <Navigation className='maximize-window'>
-                <SideNav
-                    className='maximize-window'
-                    defaultSelectedPath="home"
-                    theme={theme}
-                    onItemSelection={this.onItemSelection}>
-                    <Nav id="projects" >
-                        <Text>Projects</Text>
-                    </Nav>
-                    <Nav id="experience" >
-                        <Text>Experience</Text>
-                    </Nav>
-                    <Nav id="about">
-                        <Text>About</Text>
-                    </Nav>
-                    <Nav id="contact" >
-                        <Text>
-                            Contact
-                        </Text>
-                    </Nav>
-                </SideNav>
-            </Navigation>
+            <Menu>
+                <div id="projects" className="menu-item" onClick={this.handleClick.bind(this, 'projects')}>Projects</div>
+                <div id="experience" className="menu-item"  onClick={this.handleClick.bind(this, 'experience')}>Experience</div>
+                <div id="about" className="menu-item"  onClick={this.handleClick.bind(this, 'about')}>About</div>
+                <a id="contact" className="menu-item" href="mailto:BrandonNSaunders@gmail.com">Contact</a>
+            </Menu>
         );
     }
 }
