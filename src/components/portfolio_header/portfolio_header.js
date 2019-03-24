baa882343df31e12e612291f01a50b7b071f3494
Brandon Saunders <BrandonNSaunders@gmail.com>
2019-03-24 07:43:42 -0700

    Add header spacing

--- src/components/portfolio_header/portfolio_header.js
+++ src/components/portfolio_header/portfolio_header.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import {Container, Col, Row} from 'react-bootstrap';
 
 class PortfolioHeader extends React.Component
 {
     render()
     {
         return (
-            <div className="center-alignment" >
-                <h1 >Brandon Saunders</h1>
-            </div>
+            <Container>
+                <Row className='header-spacing' xs={12}>
+                    <Col>
+                        <h1>Brandon Saunders</h1>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
