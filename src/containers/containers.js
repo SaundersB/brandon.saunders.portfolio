cbe0052c2c312974990d29c9ad0541bacdd7df42
Brandon Saunders <BrandonNSaunders@gmail.com>
2019-03-24 20:03:02 -0700

    Remove the border line.

--- src/containers/containers.js
+++ src/containers/containers.js
@@ -4,12 +4,6 @@ export const AppContainer = styled.div`
   display: flex;
 `;
 
-export const Navigation = styled.div`
-  width: 220px;
-  flex-shrink: 0;
-  background: #fff;
-  border-right: 1px solid rgba(0, 0, 0, 0.125);
-`;
 export const Body = styled.div`
   padding: 12px;
 `;
@@ -21,13 +15,8 @@ export const Title = styled.div`
   padding: 12px;
 `;
 
-export const NavigationContainer = styled(Navigation)`
-  margin-right: 4px;
-  border: 1px solid rgba(0, 0, 0, 0.125);  
-`;
 
 export const BodyContainer = styled.div`  
   background: #fff;
   padding: 12px;
-  border: 1px solid rgba(0, 0, 0, 0.125);
 `;
