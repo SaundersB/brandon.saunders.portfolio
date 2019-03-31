4d827dbf107b275e44efcca17ac681ced6496a86
Brandon Saunders <BrandonNSaunders@gmail.com>
2019-03-30 20:41:20 -0700

    Cleanup the organizations card.

--- src/components/tag_relationships/tag_relationships.js
+++ src/components/tag_relationships/tag_relationships.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Col, Row} from 'react-bootstrap';
+import {Card, Col, ListGroup, Row} from 'react-bootstrap';
 import AnchorTagNavigationLink from './anchor_tag_navigation_link';
 import {remove_duplicates_es6} from '../../helpers/array_helpers';
 
@@ -15,18 +15,27 @@ class TagRelationships extends React.Component{
 
         return (<div>
             <Row xs={12} md={8}>
-                <Col className="text-center">
-                    Organizations {this.props.tagName} Utilized:
-                </Col>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>
+                            Organizations {this.props.tagName} Utilized
+                        </Card.Title>
+                    </Card.Body>
+                    <ListGroup>
+                        <ListGroup.Item>
+                                {this.getOrganizations().map(function(organization, i) {
+                                        return (
+                                            <div className="col" key={organization + i}>
+                                                <AnchorTagNavigationLink {...organization}/>
+                                            </div>
+                                        );
+                                    },
+                                )}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card>
             </Row>
-            {this.getOrganizations().map(function(organization, i) {
-                    return (
-                        <Row xs={12} md={8} key={organization.companyName + i} >
-                            <AnchorTagNavigationLink {...organization}/>
-                        </Row>
-                    );
-                },
-            )}
+
         </div>);
     }
 }
