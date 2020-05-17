306c5a3b46c661703ea7c74a87f43a93a6980fec
Brandon Saunders <BrandonNSaunders@gmail.com>
2020-05-17 11:32:43 -0700

    Create a video component that we can re-use with dynamic styles. Stretch video to the entire card for the project page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import Img from "gatsby-image"
 import {CardComponent} from "../components/common/card";
 import {defaultMargin, defaultPadding} from "../styles";
 import {header} from "../styles/typography";
+import {VideoComponent} from "../components/common/VideoComponent";
 
 export const query = graphql`
     query projectsQuery {
@@ -35,18 +36,17 @@ export const query = graphql`
 `
 
 
-
 function IndexPage(props: any) {
     return (
         <Layout>
             <SEO title="Home"/>
             <StaticQuery query={query}
                          render={(data) => (
-                            <>
-                                {getProjects(data)}
-                            </>
+                             <>
+                                 {getProjects(data)}
+                             </>
                          )
-                     }
+                         }
             />
         </Layout>
     );
@@ -57,6 +57,13 @@ function getProjects(data: any) {
     let projects: object[] = [];
     data.allProjectsJson.edges.forEach((project: any) => {
         const projectObj = new Project(project.node);
+        const videoUrl = project.node?.videoUrl;
+        const videoComponent = videoUrl !== null ? (
+            <VideoComponent videoSrcURL={videoUrl} videoTitle={projectObj.name}
+                            frameStyles={{minHeight: 400, width: '100%'}}
+                            wrapperStyles={{height: '100%', width: '100%'}}/>
+        ) : null;
+
         const imageOrientation = project.node.imageOrientation;
         const imageStyles = imageOrientation === 'portrait' ? {
             width: '50%',
@@ -66,15 +73,16 @@ function getProjects(data: any) {
         const imageUrl = project.node.image?.childImageSharp?.fluid;
         const imageComponent = imageUrl !== undefined ? (
             <Img fluid={imageUrl} style={{...imageStyles}}/>
-            ) : null;
+        ) : null;
         projects.push(
             <div key={projectObj.name} style={{
                 ...defaultMargin,
                 ...defaultPadding
             }}>
                 <CardComponent>
-                    <div style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <div style={{flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                         {imageComponent}
+                        {videoComponent}
                     </div>
                     <div style={{
                         ...header,
