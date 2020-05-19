1387092ee2d374011f3e23f70ce0707fef8b7d3b
Brandon Saunders <BrandonNSaunders@gmail.com>
2020-05-18 20:02:15 -0700

    Create a very basic social media connection component.

--- src/components/StaticPlaceholderComponent.tsx
+++ src/components/StaticPlaceholderComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from "./header";
+import {SocialMediaLinksComponent} from "./common/SocialMediaLinksComponent";
 
 export function StaticPlaceholderComponent(props: any){
 
@@ -7,6 +8,13 @@ export function StaticPlaceholderComponent(props: any){
     return (<div style={{
         ...props.wrapperStyles
     }}>
+        <div style={{
+            position: 'absolute',
+            top: 0,
+            right: 0
+        }}>
+            <SocialMediaLinksComponent/>
+        </div>
         <div style={{
 
         }}>
