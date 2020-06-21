68419bfdc02c2921ceb74b972ec18ac542b30c43
Brandon Saunders <BrandonNSaunders@gmail.com>
2020-06-21 12:22:29 -0700

    Center social media links.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -47,6 +47,7 @@ const Header = ({ siteTitle }: HeaderInterface) => (
             </h5>
         </div>
         <div style={{
+            alignSelf: 'center'
         }}>
             <SocialMediaLinksComponent/>
         </div>
