e8d23ca501a501c0c5315658bab5e0c6c36f25c8
Brandon Saunders <BrandonNSaunders@gmail.com>
2020-07-04 14:51:45 -0700

    Load the SCSS file from the styles folder.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,7 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import "./layout.scss"
+import "../styles/layout.scss"
 import Header from "./header";
 
 interface LayoutInterface {
