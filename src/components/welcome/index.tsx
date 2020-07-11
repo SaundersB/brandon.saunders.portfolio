d0debc46238e4b1edf052d0d3d33018b80c69b86
Brandon Saunders <BrandonNSaunders@gmail.com>
2020-07-11 11:49:11 -0700

    Break lots of className styles when they exceed 180 characters.

--- src/components/welcome/index.tsx
+++ src/components/welcome/index.tsx
@@ -8,13 +8,15 @@ export default function Welcome() {
     return (
         <div
             className={'d-flex row gradient container'}>
-                <div className="d-flex col-xs-12 col-md-5 justify-content-md-end justify-content-center align-items-md-center align-items-end p-md-2 p-xs-0">
+                <div className="d-flex col-xs-12 col-md-5 justify-content-md-end justify-content-center
+                                align-items-md-center align-items-end p-md-2 p-xs-0">
                     <div className="d-flex justify-content-end p-0">
                         <ProfilePhoto/>
                     </div>
                 </div>
                 <div
-                    className="d-flex col-xs-12 col-lg-7 col-md-7 p-0 p-md-1 p-sm-0 m-auto ml-md-0 mb-auto mt-auto text-md-left text-sm-center text-center align-self-center">
+                    className="d-flex col-xs-12 col-lg-7 col-md-7 p-0 p-md-1 p-sm-0 m-auto ml-md-0 mb-auto mt-auto
+                                text-md-left text-sm-center text-center align-self-center">
                     <div className="col align-content-between">
                         <div className={'default-text white-text header-two'}>
                             Hello. My name is<br/>
