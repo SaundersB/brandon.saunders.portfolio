2a92b556880eecefc25d4394fa2c006fb53404ee
Brandon Saunders <BrandonNSaunders@gmail.com>
2020-07-11 22:55:02 -0700

    Allow the icon to be clicked as well for a social media link.

--- src/components/common/ResumeLinkComponent.tsx
+++ src/components/common/ResumeLinkComponent.tsx
@@ -12,7 +12,7 @@ interface ResumeLinkPropsInterface {
 export default function ResumeLinkComponent(props: ResumeLinkPropsInterface){
     return (
         <div className={'row mb-md-3 justify-content-md-start justify-content-center ' + props.wrapperStyles}>
-            <FontAwesomeIcon icon={props.icon} className={'mt-1 mb-1 mr-1'}/>
+            <FontAwesomeIcon onClick={() => window.open(props.link, '_#')} icon={props.icon} color={'white'} className={'mt-1 mb-1 mr-2 hyperlink'} />
             <div className={'align-self-end'}>
                 <a className={'hyperlink sub-text'} target={'_#'} href={props.link}>{props.title}</a>
             </div>
