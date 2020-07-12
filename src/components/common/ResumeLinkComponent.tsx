eb2d53a2d0e969109c51c0e01e1b7124810269e7
Brandon Saunders <BrandonNSaunders@gmail.com>
2020-07-11 22:57:37 -0700

    Remove the Icon component.

--- src/components/common/ResumeLinkComponent.tsx
+++ src/components/common/ResumeLinkComponent.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import IconComponent from "./IconComponent";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 interface ResumeLinkPropsInterface {
