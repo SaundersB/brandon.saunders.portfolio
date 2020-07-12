8201abe04e502c761b1fdfcd279f14549aa78aa5
Brandon Saunders <BrandonNSaunders@gmail.com>
2020-07-11 22:57:11 -0700

    Allow a user to click on the entire div of a social link.

--- src/components/common/ResumeLinkComponent.tsx
+++ src/components/common/ResumeLinkComponent.tsx
@@ -11,8 +11,8 @@ interface ResumeLinkPropsInterface {
 
 export default function ResumeLinkComponent(props: ResumeLinkPropsInterface){
     return (
-        <div className={'row mb-md-3 justify-content-md-start justify-content-center ' + props.wrapperStyles}>
-            <FontAwesomeIcon onClick={() => window.open(props.link, '_#')} icon={props.icon} color={'white'} className={'mt-1 mb-1 mr-2 hyperlink'} />
+        <div onClick={() => window.open(props.link, '_#')} className={'row hyperlink mb-md-3 justify-content-md-start justify-content-center ' + props.wrapperStyles}>
+            <FontAwesomeIcon icon={props.icon} color={'white'} className={'hyperlink mt-1 mb-1 mr-2'} />
             <div className={'align-self-end'}>
                 <a className={'hyperlink sub-text'} target={'_#'} href={props.link}>{props.title}</a>
             </div>
