f27f5c1c5a38024948837163280125116329386e
Brandon Saunders <BrandonNSaunders@gmail.com>
2020-07-13 21:23:26 -0700

    Increase the header tab even more so.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -42,8 +42,8 @@ const Header = ({siteTitle}: HeaderInterface) => {
                 </Link>
             </div>
         </ul>
-        <div className=" navbar__link-toggle pr-2 m-2">
-            <FontAwesomeIcon icon={faBars} size={'lg'} color={'white'} className="fas fa-bars " onClick={() => classToggle()}/>
+        <div className="navbar__link-toggle m-auto p-auto" >
+            <FontAwesomeIcon icon={faBars} size={'2x'} color={'white'} className="fas fa-bars " onClick={() => classToggle()}/>
         </div>
     </div>);
 }
